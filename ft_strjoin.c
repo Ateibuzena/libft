cbd9dfc89b183fa2d935a5ef86f6101426735f17
Ana Zubieta Pena <azubieta@c1r1s1.42malaga.com>
2024-04-25 23:15:46 +0200

    ft_strjoin con conmentario añadido ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: azubieta <azubieta@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:33:23 by azubieta          #+#    #+#             */
-/*   Updated: 2024/04/25 23:07:03 by azubieta         ###   ########.fr       */
+/*   Updated: 2024/04/25 23:15:18 by azubieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,3 +40,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	join[i] = '\0';
 	return (join);
 }
+
+/*The ft_strjoin function concatenates two strings
+ s1 and s2 into a newly allocated string.
+Parameters:
+- s1: Pointer to the first string.
+- s2: Pointer to the second string.
+Return Value:
+- A pointer to a newly allocated string which is
+the result of the concatenation of s1 and s2.
+- If memory allocation fails, it returns NULL.
+*/
