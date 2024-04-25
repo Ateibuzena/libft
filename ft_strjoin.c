fe3fa024eb58951d1bb3d2d0a20121792aa6d1a0
Ana Zubieta Pena <azubieta@c1r1s1.42malaga.com>
2024-04-25 23:09:25 +0200

    subida ft_strjoin.c ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azubieta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: azubieta <azubieta@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:33:23 by azubieta          #+#    #+#             */
-/*   Updated: 2024/04/24 23:16:55 by azubieta         ###   ########.fr       */
+/*   Updated: 2024/04/25 23:07:03 by azubieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,28 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*join;
+	char		*join;
+	t_size		i;
+	t_size		n;
 
+	n = 0;
+	i = 0;
 	join = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (join == NULL)
 		return (NULL);
-	ft_memmove(join, s1, ft_strlen(join));
+	if (!s1 && !s2)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	while (n < ft_strlen(s2))
+	{
+		join[i] = s2[n];
+		i++;
+		n++;
+	}
+	join[i] = '\0';
 	return (join);
 }
